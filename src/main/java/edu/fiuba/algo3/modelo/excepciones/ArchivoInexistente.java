639c717f88b591cb5fb97260149e62776868becc
Nahu <diegosoles99@gmail.com>
2024-06-22 16:20:24 -0300

    update

--- src/main/java/edu/fiuba/algo3/modelo/excepciones/ArchivoInexistente.java
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/ArchivoInexistente.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.modelo.excepciones;
 
-public class ArchivoInexistente extends Exception{
+public class ArchivoInexistente extends RuntimeException {
     public ArchivoInexistente(String mensaje){
         super(mensaje);
     }
--- src/main/java/edu/fiuba/algo3/modelo/excepciones/JugadorNoEsperado.java
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/JugadorNoEsperado.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.modelo.excepciones;
 
-public class JugadorNoEsperado extends Exception {
+public class JugadorNoEsperado extends RuntimeException {
     public JugadorNoEsperado(String mensaje) {
         super(mensaje);
     }
